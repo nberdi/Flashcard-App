dd7a27b6721efc533102ab6377ddb2f198ff19f6
Nauka <berdinauryzbek@gmail.com>
2024-11-30 13:07:14 -0500

    added the Flashcard model, linked to the Module model via a foreign key; modified the create module function to request both the module name and description from the user, and store them in the database; added a route for the flashcard_module page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -11,17 +11,53 @@ class Module(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
+    flashcards = db.relationship('FlashcardApp', backref='parent_module', cascade='all, delete', lazy=True)
     
     def __repr__(self):
         return f"<Module(id={self.id}, name='{self.name}')>"
+    
+
+class FlashcardApp(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(500), nullable=False)
+    answer = db.Column(db.String(500), nullable=False)
+    module_id = db.Column(db.Integer, db.ForeignKey('module.id'), nullable=False)
+    module = db.relationship('Module', lazy=True)
+
+    def __repr__(self):
+        return f"<Flashcard(id={self.id})>"
+    
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def welcome_page():
     return render_template('welcome_page.html')
 
-@app.route('/create_module')
+@app.route('/create_module', methods=['POST', 'GET'])
 def create_module():
-    return render_template('create_module.html')
+    if request.method == 'POST':
+        module_name = request.form['module_name']
+        module_name_slug = module_name.replace(" ", "-")    # replace spaces with hyphens
+        module_description = request.form['module_description']
+        new_module = Module(name=module_name_slug, description=module_description)
+        try:
+            db.session.add(new_module)
+            db.session.commit()
+            return redirect(url_for('flashcard_module', name=new_module.name, id=new_module.id))
+        except:
+            return "There was an issue adding your question"
+    else:
+        return render_template('create_module.html')
+    
+
+@app.route('/flashcard_module/<string:name>/<int:id>')
+def flashcard_module(name, id):
+    module = Module.query.filter_by(id=id, name=name).first_or_404()
+    if module is None:
+        return "Module not found", 404  
+    return render_template('flashcard_module.html', module=module)
 
 if __name__ == '__main__':
     app.run(debug=True)
